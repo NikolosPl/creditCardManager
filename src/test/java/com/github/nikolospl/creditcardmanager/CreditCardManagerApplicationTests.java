b997c0ec7ed7df96b6b0f21dfcff9651476421ba
NikolosPl <niko2109h@gmail.com>
2026-05-28 18:47:20 +0200

    initialize credit card manager project with Gradle and Spring Boot setup

---
+++ src/test/java/com/github/nikolospl/creditcardmanager/CreditCardManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nikolospl.creditcardmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
